7469a703870b402a869419ec5bf8c041b2a75b07
Elisa Shapiro <elisashapiro@gmail.com>
2021-07-28 12:29:47 -0400

    completed assignment all tests passed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,28 @@ import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filters, setFilters] = useState("all");
+  const url = filters == "all" ? "http://localhost:3001/pets" : `http://localhost:3001/pets/?type=${filters}`
+ 
+  function onFindPetsClick(){
+    fetch(url)
+    .then(response => response.json())
+    .then(petData => setPets(petData))
+  }
+
+  function onChangeType(e){
+    setFilters(e.target.value)
+  }
+  
+  function onAdoptPet(id){
+    let updatePets = pets.map((pet) => {
+      if (pet.id === id) {
+        pet.isAdopted = true
+      } 
+      return pet
+    })
+    setPets(updatePets)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +36,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onFindPetsClick={onFindPetsClick} onChangeType={onChangeType}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} setPets={setPets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
